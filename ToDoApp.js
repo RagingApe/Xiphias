aa12cfbeeba2857de03c834b9f2e26c6eec88182
Stipan <stipan.dizdar@gmail.com>
2024-08-15 11:28:46 +0200

    webcomponents warming up

--- ToDoApp.js
+++ ToDoApp.js
@@ -1,9 +1,40 @@
-class ToDoItem extends HTMLElement {
+const template = document.createElement("template")
+template.innerHTML = `
+<style> 
+    label {
+        color: red;
+        display: block;
+    }
+</style>
+
+<label>
+    <input type="checkbox" />
+    <slot></slot>
+</label>
+`
+
+class ToDoEl extends HTMLElement {
     constructor(){
         super()
         const shadow = this.attachShadow({ mode: "open"})
-        this.innerHTML = `<h3>${this.innerText}</h3>`
+        shadow.append(template.content.cloneNode(true))
+        this.checkbox =shadow.querySelector("input")
+    }
+
+    static get observedAttributes() {
+        return ["checked"]
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "checked") this.updateChecked(newValue)
+    }
+
+    updateChecked(value) {
+        this.checkbox.checked = value != null && value !== "false"
     }
 }
 
-customElements.define('todo-el', ToDoItem)
+customElements.define('todo-el', ToDoEl)
+
+//Add input filed for toDo el's
+
+//Delete all button
